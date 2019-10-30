df4192ee04ca75b85b669b13607e950011e3e989
github username <github email address>
2019-10-30 16:32:09 -0500

    generate resources

--- coffee-cups-api/db/migrate/20191030211727_create_coffees.rb
+++ coffee-cups-api/db/migrate/20191030211727_create_coffees.rb
@@ -1,6 +1,7 @@
 class CreateCoffees < ActiveRecord::Migration[5.2]
   def change
     create_table :coffees do |t|
+      t.string :name
       t.references :roaster, foreign_key: true
 
       t.timestamps
