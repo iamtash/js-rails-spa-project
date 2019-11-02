e49d1317e1f81f4e06e0432df930fde5964e802c
github username <natashachernis@gmail.com>
2019-11-02 12:34:45 -0700

    make coffees data available

--- coffee-cups-api/app/controllers/coffees_controller.rb
+++ coffee-cups-api/app/controllers/coffees_controller.rb
@@ -1,2 +1,6 @@
 class CoffeesController < ApplicationController
+    def index
+        roaster = Roaster.find_by(id: params[:roaster_id])
+        render json: ObjSerializer.new(roaster.coffees).to_serialized_json
+    end
 end
