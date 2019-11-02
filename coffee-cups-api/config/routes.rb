843e48487f0430e65a90525fb2157f51d135a9a9
github username <natashachernis@gmail.com>
2019-11-02 12:35:52 -0700

    add coffee index route nested under roasters resource

--- coffee-cups-api/config/routes.rb
+++ coffee-cups-api/config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get 'cups' => 'cups#index'
   get 'brews' => 'brews#index'
   get 'roasters' => 'roasters#index'
+
+  resources :roasters, only: [:index] do
+    resources :coffees, only: [:index]
+  end
 end
