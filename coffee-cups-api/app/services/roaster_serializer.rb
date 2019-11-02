b1d3ecad0b858e36e247affc529a06f9379bf2a5
github username <natashachernis@gmail.com>
2019-11-02 12:37:12 -0700

    abstract roaster serializer class

--- coffee-cups-api/app/services/roaster_serializer.rb
+++
@@ -1,9 +0,0 @@
-class RoasterSerializer 
-    def initialize(roaster_object)
-        @roaster = roaster_object
-    end
-
-    def to_serialized_json
-        @roaster.to_json(only: [:id, :name])
-    end
-end
