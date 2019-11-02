fedf237fa97a6aaa9b710693e68e67e62ba5b98c
github username <natashachernis@gmail.com>
2019-11-01 17:19:20 -0700

    set up roasters data / fetch

--- coffee-cups-api/app/controllers/roasters_controller.rb
+++ coffee-cups-api/app/controllers/roasters_controller.rb
@@ -1,2 +1,6 @@
 class RoastersController < ApplicationController
+    def index
+        roasters = Roaster.all
+        render json: RoasterSerializer.new(roasters).to_serialized_json
+    end
 end
