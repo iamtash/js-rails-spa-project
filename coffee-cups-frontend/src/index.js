67ce5b93b207b7a65d000bf01b94d180bb4418e0
github username <natashachernis@gmail.com>
2019-11-01 17:24:26 -0700

    set up initial page rendering

--- coffee-cups-frontend/src/index.js
+++ coffee-cups-frontend/src/index.js
@@ -1,7 +1,34 @@
 const BASE_URL = "http://localhost:3000"
 const CUPS_URL = `${BASE_URL}/cups`
+const BREWS_URL = `${BASE_URL}/brews`
+const ROASTERS_URL = `${BASE_URL}/roasters`
 
-fetch(CUPS_URL)
-    .then(resp => resp.json())
-    .then(resp => console.log(resp))
-    
+let outerContainer = document.querySelector('div.outer-container')
+let cups
+
+document.addEventListener('DOMContentLoaded', function() {
+    outerContainer.appendChild(newCupButton())
+    fetch(CUPS_URL)
+        .then(resp => resp.json())
+        .then(json => Cup.buildCupObjects(json))
+        .then(cupObjs => {
+            Cup.renderCups(cupObjs)
+            cups = cupObjs
+        })
+})
+
+function newCupButton() {
+    let button = document.createElement('button')
+    button.textContent = 'Sip a cup'
+    button.addEventListener('click', function() {
+        outerContainer.innerHTML = ''
+        outerContainer.appendChild(generateNewCupForm())
+    })
+    return button
+}
+
+
+
+
+
+
