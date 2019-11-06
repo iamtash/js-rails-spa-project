76700b579013220aef480d2cb0f9e3b6ba7d4ce1
github username <natashachernis@gmail.com>
2019-11-05 16:17:15 -0800

    handle new cup submission in controller

--- coffee-cups-api/app/controllers/cups_controller.rb
+++ coffee-cups-api/app/controllers/cups_controller.rb
@@ -3,4 +3,23 @@ class CupsController < ApplicationController
         cups = Cup.all
         render json: CupSerializer.new(cups).to_serialized_json
     end
+
+    def create
+        cup = Cup.new(cup_params)
+        if cup.save
+            render json: CupSerializer.new(cup).to_serialized_json
+        end
+    end
+
+    private 
+        def cup_params
+            params.require(:cup).permit(
+                :user_id,
+                :brew_id,
+                :coffee_id,
+                rating_attributes: [
+                    :rating
+                ]
+            )
+        end
 end
