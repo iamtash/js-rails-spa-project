465efce3f5cc1c09c64d60dafd03e75c308a9b8a
github username <natashachernis@gmail.com>
2019-11-05 16:18:13 -0800

    define nested attributes rating constructor in cup model

--- coffee-cups-api/app/models/cup.rb
+++ coffee-cups-api/app/models/cup.rb
@@ -3,6 +3,11 @@ class Cup < ApplicationRecord
   belongs_to :coffee
   belongs_to :brew
   belongs_to :rating
-  validates :user, presence: true
-  validates_associated :coffee
+  # validates :user, presence: true
+  # validates_associated :coffee
+
+  def rating_attributes=(rating_attributes)
+      self.rating = Rating.new(rating_attributes)
+  end
+
 end
