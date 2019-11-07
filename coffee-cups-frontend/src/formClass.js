6651c090f1b8705a1b82e4fa888c1eae851f0734
github username <natashachernis@gmail.com>
2019-11-06 17:57:10 -0800

    move form assembly logic to parent Form class; customize submit button text

--- coffee-cups-frontend/src/formClass.js
+++ coffee-cups-frontend/src/formClass.js
@@ -16,6 +16,12 @@ class Form {
         this.inputFields.id = 'input-fields'
     }
 
+    assembleFormElements(submitNewObjFunc) {
+        this.fieldset.appendChild(this.inputFields)
+        this.fieldset.appendChild(this.buildSubmit(submitNewObjFunc))
+        this.formNode.appendChild(this.fieldset)
+    }
+
     buildFormNode() {
         let form = document.createElement('form')
         form.id = `new-${this.type}`
@@ -38,11 +44,14 @@ class Form {
         return fieldset
     }
 
-    static buildSubmit(submitNewObjFunc) {
+    buildSubmit(submitNewObjFunc) {
         let submit = document.createElement('input')
         submit.type = 'submit'
-        submit.value = 'Post'
+        if (this.type === 'cup') submit.value = 'Post'
+        else if (this.type === 'user') submit.value = 'Sign up'
         submit.addEventListener('click', (e) => submitNewObjFunc(e))
         return submit
     }
+
+
 }
