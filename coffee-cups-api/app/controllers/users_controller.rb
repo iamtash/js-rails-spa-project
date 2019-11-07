9b7f287302268ae020a2285e71fa669ec4b8d9fc
github username <natashachernis@gmail.com>
2019-11-06 17:54:37 -0800

    write User creation controller logic

--- coffee-cups-api/app/controllers/users_controller.rb
+++ coffee-cups-api/app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: ObjSerializer.new(user).to_serialized_json
+        end
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:name, :email, :password)
+        end
 end
