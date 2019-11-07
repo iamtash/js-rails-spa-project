c61140b635c6571bf5f236b33e1043103b9a9e08
github username <natashachernis@gmail.com>
2019-11-06 17:58:22 -0800

    first render user signup form and conditionally render cups after

--- coffee-cups-frontend/src/index.js
+++ coffee-cups-frontend/src/index.js
@@ -2,20 +2,32 @@ const BASE_URL = "http://localhost:3000"
 const CUPS_URL = `${BASE_URL}/cups`
 const BREWS_URL = `${BASE_URL}/brews`
 const ROASTERS_URL = `${BASE_URL}/roasters`
+const USERS_URL = `${BASE_URL}/users`
 
 let currentUser
 let outerContainer = document.querySelector('div.outer-container')
 let cupsContainer
 
 document.addEventListener('DOMContentLoaded', function() {
-    outerContainer.appendChild(newCupButton())
-    generateCupsWrapper()
-    fetch(CUPS_URL)
-        .then(resp => resp.json())
-        .then(json => Cup.buildCupObjects(json))
-        .then(cupObjs => Cup.renderCups(cupObjs))
-        .catch(error => console.log(error.message))
+    renderUserSignup()
 })
+
+function renderUserSignup() {
+    let newUserForm = new NewUserForm('user')
+    outerContainer.appendChild(newUserForm.formNode)
+}
+
+function getCups() {
+    if (currentUser.id) {
+        outerContainer.appendChild(newCupButton())
+        generateCupsWrapper()
+        fetch(CUPS_URL)
+            .then(resp => resp.json())
+            .then(json => Cup.buildCupObjects(json))
+            .then(cupObjs => Cup.renderCups(cupObjs))
+            .catch(error => console.log(error.message))
+    } else renderUserSignup()
+}
 
 function newCupButton() {
     let button = document.createElement('button')
@@ -35,9 +47,3 @@ function generateCupsWrapper() {
     cupsContainer.className = 'cups-wrapper'
     outerContainer.appendChild(cupsContainer)
 }
-
-
-
-
-
-
