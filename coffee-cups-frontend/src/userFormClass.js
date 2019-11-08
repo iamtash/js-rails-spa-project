797258c014dcf39270287edb467475e91cdd3f93
github username <natashachernis@gmail.com>
2019-11-07 22:14:14 -0800

    build in option to toggle b/w signup and login forms

--- coffee-cups-frontend/src/userFormClass.js
+++ coffee-cups-frontend/src/userFormClass.js
@@ -19,6 +19,7 @@ class UserForm extends Form {
 
     static submitNewObj(e) {
         super.submitNewObj(e)
+        document.querySelector('div.exit-option').remove()
     }
 
     static userConfigObjBody(e, fields) {
@@ -27,15 +28,42 @@ class UserForm extends Form {
         return { body: JSON.stringify(data) }
     }
 
-    static fetchUser(url, configObj, catchFunc) {
+    static fetchUser(url, configObj, catchFunc, failure) {
         fetch(url, configObj)
         .then(resp => resp.json())
-        .then(user => currentUser = user) 
+        .then(user => currentUser = user)
         .then(() => getCups())
         .catch(error => {
+            console.log(`${failure} FAILED`)
             console.log(error.message)
             catchFunc()
         })
     }
+
+    get exitOption() { // refactor this
+        let exitOption = document.createElement('div')
+        exitOption.className = 'exit-option'
+        let exitText = document.createElement('h4')
+        let button = document.createElement('button')
+        button.className = 'exit-option'
+        button.addEventListener('click', () => {
+            if (this.type === 'user') renderUserLogin()
+            else if (this.type === 'session') renderUserSignup()
+        })
+
+        if (this.type === 'user') {
+            exitText.innerText = 'Already have an account?'
+            button.textContent = 'Log in'
+        } else if (this.type === 'session') {
+            exitText.innerText = "Don't have an account?"
+            button.textContent = 'Sign up'
+        }
+
+        exitOption.appendChild(exitText)
+        exitOption.appendChild(button)
+        return exitOption
+    }
 }
+
+
 
