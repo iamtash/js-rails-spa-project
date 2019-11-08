2855ac051397840d86176c29a95f0ea0e079567e
github username <natashachernis@gmail.com>
2019-11-08 08:51:06 -0800

    consolidate user form child classes

--- coffee-cups-frontend/src/newUserForm.js
+++
@@ -1,7 +0,0 @@
-class NewUserForm extends UserForm {
-    static submitNewObj(e) {
-        super.submitNewObj(e)
-        let configObj = Object.assign({}, newObjConfigObj, UserForm.userConfigObjBody(e, signupFields))
-        UserForm.fetchUser(USERS_URL, configObj, renderUserSignup, 'SIGNUP')
-    }
-}
--- coffee-cups-frontend/src/userFormChildren.js
+++ coffee-cups-frontend/src/userFormChildren.js
@@ -1,3 +1,11 @@
+class NewUserForm extends UserForm {
+    static submitNewObj(e) {
+        super.submitNewObj(e)
+        let configObj = Object.assign({}, newObjConfigObj, UserForm.userConfigObjBody(e, signupFields))
+        UserForm.fetchUser(USERS_URL, configObj, renderUserSignup, 'SIGNUP')
+    }
+}
+
 class NewSessionForm extends UserForm {
     static submitNewObj(e) {
         super.submitNewObj(e)
