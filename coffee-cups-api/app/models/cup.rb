d088f6fc4a8d30f847cea7825990ad368d9f7e00
github username <natashachernis@gmail.com>
2019-11-11 00:53:20 -0600

    forbid a non-rated cup to be saved

--- coffee-cups-api/app/models/cup.rb
+++ coffee-cups-api/app/models/cup.rb
@@ -5,6 +5,7 @@ class Cup < ApplicationRecord
   belongs_to :rating
   # validates :user, presence: true
   # validates_associated :coffee
+  validates_associated :rating
 
   def rating_attributes=(rating_attributes)
       self.rating = Rating.new(rating_attributes)
